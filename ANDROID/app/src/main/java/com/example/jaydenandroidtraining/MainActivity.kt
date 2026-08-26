7abed8e0dbd057e0ccb449b9ec94ef817be0b013
Jean-Luc <st10435246@myemeris.edu.za>
2026-08-26 16:52:04 +0200

    change char to 8

--- ANDROID/app/src/main/java/com/example/jaydenandroidtraining/MainActivity.kt
+++ ANDROID/app/src/main/java/com/example/jaydenandroidtraining/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        //int bob = 3
+        //int bob = 8
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
