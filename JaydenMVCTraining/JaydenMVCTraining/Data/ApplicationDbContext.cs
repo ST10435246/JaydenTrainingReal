79e1b6d913a73f9217b0db77c4ddcb9e8fd64042
Jean-Luc <st10435246@myemeris.edu.za>
2026-08-26 15:16:20 +0200

    initial MVC commit

---
+++ JaydenMVCTraining/JaydenMVCTraining/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JaydenMVCTraining.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ JaydenMVCTraining/JaydenMVCTraining/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JaydenMVCTraining.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
